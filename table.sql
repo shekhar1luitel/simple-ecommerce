310d0f9d1cb5dd6e3406c186127d803d4377c0b3
Shekhar Luitel <shekhar1luitel@gmail.com>
2024-03-11 00:37:54 +0545

    Database updated

--- table.sql
+++ table.sql
@@ -3,15 +3,39 @@ CREATE TABLE
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `email` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
+    `role` enum('user', 'admin') DEFAULT 'user',
     PRIMARY KEY (`id`),
     UNIQUE KEY `email` (`email`)
   )
-CREATE TABLE products (
-    id INT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    image VARCHAR(255),
-    price DECIMAL(10, 2) NOT NULL,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+
+  CREATE TABLE
+  `products` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `description` text DEFAULT NULL,
+    `image` varchar(255) DEFAULT NULL,
+    `price` decimal(10, 2) NOT NULL,
+    `user_id` int(11) DEFAULT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `contact` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `products_ibfk_1` (`user_id`),
+    CONSTRAINT `products_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  )
+  CREATE TABLE
+    `cart_items` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `product_id` int(11) NOT NULL,
+      `user_id` int(11) DEFAULT NULL,
+      `quantity` int(11) NOT NULL DEFAULT 1,
+      `status` varchar(255) DEFAULT 'in_cart',
+      `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+      `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+      `client_contact` varchar(255) DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      KEY `cart_items_ibfk_2` (`user_id`),
+      KEY `cart_items_idfk_1` (`product_id`),
+      CONSTRAINT `cart_items_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+      CONSTRAINT `cart_items_idfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE
+    )
